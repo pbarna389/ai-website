75f1b6c6a796a48f3b85bae039b263f467a0abec
pbarna <pbarna389@gmail.com>
2024-11-11 17:10:14 +0100

    added semi absoulte imports, needs to figure out how it works with the new vite setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import { Header } from "./components"
+
 export const App = () => {
 	return (
 		<div>
-			<h1>
-				The application will be here
-			</h1>
+			<Header />
 		</div>
 	)
 }
-
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return (
+    <h1>The application will be here</h1>
+  )
+}
