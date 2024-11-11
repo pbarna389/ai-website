5067f1cf8a7dac069a60a563d07261cd43d36fb2
pbarna <pbarna389@gmail.com>
2024-11-11 18:45:36 +0100

    fixed absolute import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
-})
+	plugins: [react()],
+	resolve: {
+    alias: {
+      "@assets": resolve(__dirname, "./src/assets/*"),
+      "@components": resolve(__dirname, "./src/components/*"),
+    },
+}})
