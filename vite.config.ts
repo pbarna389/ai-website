cf8f7e051feb62d8f8316ce588b31396ddef5d63
pbarna <pbarna389@gmail.com>
2024-11-11 18:50:22 +0100

    fixed resolve-related problems with absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@assets': resolve(__dirname, './src/assets/*'),
-			'@components': resolve(__dirname, './src/components/*')
-		}
-	}
+	plugins: [react(), tsconfigPaths()],
 })
