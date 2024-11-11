398dc859dfc38399871b1623bcd4b44cd7cef421
pbarna <pbarna389@gmail.com>
2024-11-11 18:46:02 +0100

    added remaining changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from "path";
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-    alias: {
-      "@assets": resolve(__dirname, "./src/assets/*"),
-      "@components": resolve(__dirname, "./src/components/*"),
-    },
-}})
+		alias: {
+			'@assets': resolve(__dirname, './src/assets/*'),
+			'@components': resolve(__dirname, './src/components/*')
+		}
+	}
+})
