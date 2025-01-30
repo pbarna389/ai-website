7da55f4d2e94582a60a8feaa5c347f3e33dacd4e
PB <pbarna389@gmail.com>
2025-01-30 23:30:39 +0100

    04/headerDesign - added first iteration of the animations

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,17 @@
+import girl1 from './assets/girl1.png'
+import girl2 from './assets/girl2.png'
+import girl3 from './assets/girl3.png'
+import girl4 from './assets/girl4.png'
+
+import './styles.css'
+
 export const Header = () => {
-	return <header>Header</header>
+	return (
+		<header>
+			<img className={'img left first'} src={girl1} />
+			<img className={'img left second'} src={girl2} />
+			<img className={'img right first'} src={girl3} />
+			<img className={'img right second'} src={girl4} />
+		</header>
+	)
 }
