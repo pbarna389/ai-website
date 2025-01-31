81b429e1de400d128c7107bda2f8cf35d9c5af6f
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2025-01-31 20:04:22 +0100

    04/headerDesignBase - added the mainline for the header (#4)
    * 04/headerDesign - added assets for header
    
    * 04/headerDesign - added first iteration of the animations
    
    * 04/headerDesign - animtions on imgs complete
    
    * 04/headerDesign - minor adjustments
    
    * 04/headerDesign - added basic logo
    
    * 04/headerDesign - added more separating classnames and the base for intro and animation control
    
    * 04/headerDesign - minor syntax correction
    
    * 04/headerDesign - intro done
    
    * 04/headerDesign: fine-tuned the svg stroke width

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { RouterProvider } from 'react-router'
 import { Footer, Header } from '@components'
 import { router } from '@router'
 
+import 'styles.css'
+
 export const App = () => {
 	return (
 		<>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,32 @@
+import { useCallback, useState } from 'react'
+
+import girl1 from './assets/girl1.png'
+import girl2 from './assets/girl2.png'
+import girl3 from './assets/girl3.png'
+import girl4 from './assets/girl4.png'
+
+import { Logo } from './components'
+
+import './styles.css'
+
 export const Header = () => {
-	return <header>Header</header>
+	const [isIntroFinished, setIntroFinished] = useState<boolean>(false)
+
+	const handleIntro = useCallback(() => {
+		setTimeout(() => {
+			setIntroFinished(true)
+		}, 5000)
+	}, [])
+
+	handleIntro()
+
+	return (
+		<header className={`${isIntroFinished && 'animated'}`}>
+			<Logo />
+			<img className={`img left first ${isIntroFinished && 'animated'}`} src={girl1} />
+			<img className={`img left second ${isIntroFinished ? 'animated' : 'intro-anim'}`} src={girl2} />
+			<img className={`img right first ${isIntroFinished && 'animated'}`} src={girl3} />
+			<img className={`img right second ${isIntroFinished ? 'animated' : 'intro-anim'}`} src={girl4} />
+		</header>
+	)
 }
