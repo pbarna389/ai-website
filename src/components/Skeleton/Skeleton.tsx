f1b286b6471a3ca2333d797aa1d24bb29ea67335
PB <pbarna389@gmail.com>
2025-02-12 17:47:06 +0100

    07/design-layouts: skeleton adjusted to be more readable

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -3,7 +3,7 @@ import type { SkeletonProps } from './types'
 import './styles.css'
 
 export const Skeleton = ({ amount, classNames }: SkeletonProps) => {
-	const skeletonCount = Array.from({ length: amount }, (_, i) => `insta-skel-${i + 1}`)
+	const skeletonCount = Array.from({ length: amount }, (_, i) => `${classNames}-skeleton-${i + 1}`)
 
 	return skeletonCount.map((el) => <div key={el} className={`skeleton ${classNames}`} />)
 }
