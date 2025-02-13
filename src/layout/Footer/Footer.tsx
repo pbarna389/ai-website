e58956b7029cb7d928bb29906703e01ced6c4986
PB <pbarna389@gmail.com>
2025-02-13 12:15:00 +0100

    08/footer - added react-icons package

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
 		<footer className="flex flex-column">
 			<div className="footer-main flex flex-sp-ev flex-center">
 				<Logo isIntroFinished height={80} />
-				<p>links will be here</p>
+				<p>Socials will be here</p>
 			</div>
 			<div className="flex flex-center">Site be me</div>
 			<div className="flex flex-center">Copyright</div>
