7f34e5a7c8472dea9800a3221514c9f941d0df87
PB <pbarna389@gmail.com>
2025-02-13 11:05:48 +0100

    07/design-layouts: added some hover styling for the navbar

--- src/components/Navbar/utils/utils.ts
+++ src/components/Navbar/utils/utils.ts
@@ -1,5 +1,5 @@
 export const pageUrls = ['Youtube', 'Instagram']
 
-const scrollOptions: ScrollToOptions = { top: 0, left: 0, behavior: 'smooth' }
+const scrollOptions: ScrollToOptions = { top: 0, left: 0, behavior: 'instant' }
 
 export const scrollToTop = () => window.scrollTo(scrollOptions)
