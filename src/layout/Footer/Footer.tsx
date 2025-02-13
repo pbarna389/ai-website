9cf013d248617329085558aeff20d932e60aec50
PB <pbarna389@gmail.com>
2025-02-13 14:47:53 +0100

    08/footer - fixed the footer logo

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -7,7 +7,7 @@ import './styles.css'
 export const Footer = () => {
 	return (
 		<footer className="flex flex-column">
-			<div className="footer-main flex flex-sp-ev flex-center">
+			<div className="flex flex-sp-ev flex-center footer-main">
 				<Logo isIntroFinished height={80} />
 				<SocialLinks />
 			</div>
