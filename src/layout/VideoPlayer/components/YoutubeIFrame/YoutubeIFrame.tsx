c42c5a0c836a4c45f401c289ca0daf5a37a26660
PB <pbarna389@gmail.com>
2025-02-17 19:43:32 +0100

    13/newVideoPlayer - added full screen support for YT, made more accessible on desktop - no mobile yet

--- src/layout/VideoPlayer/components/YoutubeIFrame/YoutubeIFrame.tsx
+++ src/layout/VideoPlayer/components/YoutubeIFrame/YoutubeIFrame.tsx
@@ -6,8 +6,9 @@ export const YoutubeIFrame = ({ link }: YoutubeIFrameProps) => {
 	return (
 		<iframe
 			src={`https://www.youtube.com/embed/${link}?enablejsapi=1&autoplay=1&modestbranding=1&rel=0`}
-			allow="accelerometer; autoplay; encrypted-media; gyroscope;"
+			allow="accelerometer; autoplay; encrypted-media; gyroscope; fullscreen;"
 			sandbox="allow-scripts allow-same-origin allow-presentation"
+			allowFullScreen
 		/>
 	)
 }
